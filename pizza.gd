619626ede1db2b72981e7cbed914439a8e3078a7
Cube Number Zero <cubenumberzero@gmail.com>
2025-01-30 11:17:17 -0500

    another

--- pizza.gd
+++ pizza.gd
@@ -1,1 +1,12 @@
 extends Object
+class_name Pizza
+## Class for pizzas
+
+## The current temperature of the pizza
+var temperature: float
+
+func _init(starting_temp: float) -> void:
+	temperature = starting_temp
+
+func process_temperature(ambient_temperature: float, delta_time: float) -> void:
+	temperature += Temperature.get_delta_temperature(temperature, ambient_temperature, delta_time)
