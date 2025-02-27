93179c3819585b86a75947a2a616d74af6fea750
Yvette Smith <noellebsmith2@gmail.com>
2025-02-27 10:02:17 -0500

    Made camera fixed a distance above the player

--- scripts/camera_rig.gd
+++ scripts/camera_rig.gd
@@ -1,26 +1,9 @@
 extends Node3D
 
-var pitch:float = 0.0
-var yaw:float = 0.0
-var CameraRotation = 0
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+@onready var camera : Camera3D = get_child(0)
+@onready var player : Node3D = $".."
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
-
-func _unhandled_input(event):
-	if event is InputEventMouseMotion:
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-		
-		yaw -= event.relative.x * 0.05
-		CameraRotation = yaw
-		rotation_degrees = Vector3(0, yaw, 0)
-		pitch -= event.relative.y * 0.08
-		if pitch > -90:			pitch = -90
-		if pitch < -60:			pitch = -60
-		$Camera3D.rotation_degrees = Vector3(pitch, 0, 0)
+	global_position = player.global_position
+	global_rotation = Vector3.ZERO
